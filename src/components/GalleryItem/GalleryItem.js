949836d2caa48861ac3648d1346ffe3eb53a63ca
Steve Simonson <srsimonson@gmail.com>
2020-06-06 13:56:52 -0500

    conditional rendering complete

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,17 +1,55 @@
 import React, {Component} from 'react';
+// import axios from 'axios';
 
 class GalleryItem extends Component {
-    
+
+    state = {
+        toggleValue: true
+    }
+
+    // updateLikes = (item) => {
+    //     console.log('item', item);
+    //     let likes = item.likes;
+    //     console.log('in updateLikes');
+        
+    //     axios.put(`/gallery/like/${item.id}` )
+    //         .then(response => {
+    //         // this.props.displayGallery();
+    //         console.log('item.id', item.id, likes);
+    //         })
+    //         .catch(error => {
+    //         alert(`Like button not working`);
+    //         console.log('ERROR with PUT in app.js', error);
+    //         })
+    //     }
+
+    clickTest = (item) => {
+        // let item = this.props.item
+        console.log('clickTest', item.id);
+    }
+
+    handleClick = () => {
+        this.setState({
+            toggleValue: !this.state.toggleValue
+        })
+    }
+
     render() {
+        // let item = this.props.item
         return (
             <div>
-                <p>hi</p>
-                
-                {this.props.updateLikes}
+                <button className="painting" onClick={this.handleClick}>
+                    {this.state.toggleValue 
+                        ? <img className="resize" src={this.props.item.path}/> 
+                        : <p className="resize">{this.props.item.description}</p>}
+                </button>  
+                <br/>
+                <button onClick={this.clickTest}>love it!</button>
+                <p>{this.props.item.likes} people love this!</p>
+                {/* {this.props.updateLikes} */}
             </div>
         )
     }
-
 }
 
 export default GalleryItem;
--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -1,40 +1,14 @@
 import React, {Component} from 'react';
 import GalleryItem from '../GalleryItem/GalleryItem';
-import axios from 'axios';
 
 class GalleryList extends Component {
-    state = {
-        toggleValue: false
-    }
-
-    clickEvent = () =>{
-        console.log('like button clicked');      
-    }
-
-    updateLikes = (item) => {
-        console.log('in updateLikes');
-        
-        axios.put(`/gallery/like/${item.id}`, {} )
-            .then(response => {
-            this.getGallery()
-            })
-            .catch(error => {
-            alert(`Like button not working`);
-            console.log('ERROR with PUT in app.js', error);
-            })
-        }
 
     render() {
         return (
             <>
                 {this.props.displayGallery.map((item) => {
                     return <div className="container" key={item.id}>
-                                <img className="resize" src={item.path} alt={item.description}/>
-                                <br/>
-                                <button onClick={this.clickEvent}>love it!</button>
-                                <p>{item.likes} people love this!</p>
-                                <GalleryItem updateLikes={this.updateLikes}/>
-
+                                <GalleryItem key={item.id} item={item}/>
                             </div>
                 })}
             </>
