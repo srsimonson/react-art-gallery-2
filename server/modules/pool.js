516f9736765c738d0b4e35f10a848060f4f21f72
Steve Simonson <srsimonson@gmail.com>
2020-07-07 12:22:41 -0500

    pool updated for db

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,25 +1,54 @@
-// Node Module that will connect to postgesql
 const pg = require('pg');
-
-// Setup PG to connect to the database
 const Pool = pg.Pool;
+const url = require('url');
+let config = {};
 
-const pool = new Pool({
-    database: 'react_gallery', // database name (this will change)
-    host: 'localhost', // where to find the database
-    port: 5432,        // port for finding the database
-    max: 10,           // max number of connections for the pool
-    idleTimeoutMillis: 30000 // 30 seconds before timeout/cancel query
-});
+if (process.env.DATABASE_URL) {
+    // Heroku gives a url, not a connection object
+    // https://github.com/brianc/node-pg-pool
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
+  
+    config = {
+      user: auth[0],
+      password: auth[1],
+      host: params.hostname,
+      port: params.port,
+      database: params.pathname.split('/')[1],
+      ssl: { rejectUnauthorized: false },
+      max: 10, // max number of clients in the pool
+      idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
+  } else {
+    config = {
+      host: 'localhost', // Server hosting the postgres database
+      port: 5432, // env var: PGPORT
+      database: 'react_gallery', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+      max: 10, // max number of clients in the pool
+      idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };
+  }
+  
+const pool = new pg.Pool(config);
+// const pool = new Pool({
+//    database: process.env.DATABASE_NAME || 'weekend-to-do-app',
+//    host: 'localhost',
+//    port: 5432,
+//    max: 10,
+//    idleTimeoutMillis: 30000
+// });
 
-// Listener setup on the pool isn't required, 
-// but can be super handy for troubleshooting.
 pool.on('connect', () => {
-    console.log('Connected to the database');
+    console.log('Postgres connected');
 });
 
-pool.on('error', (error) => {
-    console.log('Error with database pool', error);
-});
+pool.on('error', (err) => {
+    console.log('Unexpected error on idle client', err);
+    process.exit(-1);
+  });
+
+// pool.on('error', (error) => {
+//     console.log('ERROR with postgres pool', error);
+// });
 
 module.exports = pool;
